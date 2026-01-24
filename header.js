1cb98c1edc64d426e1658cbeabe4cdfd3a7dce42
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2026-01-24 23:52:44 +0100

    Feat: add Now Web

---
+++ header.js
@@ -0,0 +1,19 @@
+document.getElementById("header").innerHTML = `
+<header class="site-header">
+    <div class="container nav">
+
+        <div class="brand">
+            <a href="index.html" class="logo" aria-label="Craftunity Netzwerk">
+                <img src="images/logo.png" alt="Craftunity Logo">
+            </a>
+        </div>
+
+        <nav>
+            <a href="index.html" class="nav-link">Home</a>
+            <a href="news.html" class="nav-link">News</a>
+            <a href="ranks.html" class="nav-link">Ränge</a>
+        </nav>
+
+    </div>
+</header>
+`;
