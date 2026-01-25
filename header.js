a2874f6c240fc04053dc76c72d37cdaef7c5b404
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2026-01-25 17:42:00 +0100

    Feat: Fix Name from Web

--- header.js
+++ header.js
@@ -3,13 +3,13 @@ document.getElementById("header").innerHTML = `
     <div class="container nav">
 
         <div class="brand">
-            <a href="Home.html" class="logo" aria-label="Craftunity Netzwerk">
+            <a href="Index.html" class="logo" aria-label="Craftunity Netzwerk">
                 <img src="images/logo.png" alt="Craftunity Logo">
             </a>
         </div>
 
         <nav>
-            <a href="Home.html" class="nav-link">Home</a>
+            <a href="Index.html" class="nav-link">Home</a>
             <a href="News.html" class="nav-link">News</a>
             <a href="Ranks.html" class="nav-link">Ranks</a>
         </nav>
